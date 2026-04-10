f3c9ee83d67245422d36f11ed0ba877d88072238
Joshua Mabawonku <drakejoshua086@gmail.com>
2026-04-10 05:57:50 +0100

    feat( routing ): update the AppLayout markup to include neccessary content and preparations for styling
    This commit makes updates to the markup of the AppLayout to include the
    neccessary content such as the top bar with the logo, theme toggle, etc
    and the side navigation with the neccessary links. This also includes
    preparations for styling such as adding proper decriptive comments to the
    elements and importing the necessary assets such as the bitcoin image for
    the avatar. This commit does not include any styling changes, but it sets up the
    structure and content for the AppLayout to be styled in the future.

--- src/shared/layouts/AppLayout.jsx
+++ src/shared/layouts/AppLayout.jsx
@@ -1,12 +1,114 @@
 import React from 'react'
-import { Outlet } from 'react-router-dom'
+import { NavLink, Outlet } from 'react-router-dom'
+import Logo from "../../shared/components/Logo";
+import { FaBars, FaGear, FaListCheck, FaMagnifyingGlass, FaStar, FaUser, FaWallet } from 'react-icons/fa6';
+import ThemeButton from '../components/ThemeButton';
+import { Avatar, DropdownMenu } from 'radix-ui';
+import bitcoinImage from "../../assets/Design/bitcoin.png"
 
 export default function AppLayout() {
   return (
     <div>
-      This is the app layout
+      {/* top bar */}
+      <div>
+        {/* nav toggle */}
+        <button>
+          <FaBars />
+        </button>
 
-      <Outlet />
+        {/* logo */}
+        <Logo />
+
+        {/* search  */}
+        <div>
+            <FaMagnifyingGlass />
+            <input type="text" placeholder='Search...' />
+        </div>
+
+        <ThemeButton />
+
+        <DropdownMenu.Root>
+            <DropdownMenu.Trigger asChild>
+                <Avatar.Root>
+                    <Avatar.Image src={bitcoinImage} alt='User profile photo'/>
+                    <Avatar.Fallback>
+                        BTC
+                    </Avatar.Fallback>
+                </Avatar.Root>
+            </DropdownMenu.Trigger>
+
+            <DropdownMenu.Content>
+                {/* dropdown details card */}
+                <div>
+                    <Avatar.Root>
+                        <Avatar.Image src={bitcoinImage} alt='User profile photo'/>
+                        <Avatar.Fallback>
+                            BTC
+                        </Avatar.Fallback>
+                    </Avatar.Root>
+
+                    <div>
+                        <span>
+                            Satoshi Nakamoto
+                        </span>
+
+                        <span>
+                            satoshi@bitcoin.com
+                        </span>
+                    </div>
+                </div>
+
+                {/* dropdown links */}
+                <div>
+                    <DropdownMenu.Item>Profile</DropdownMenu.Item>
+                    <DropdownMenu.Item>Settings</DropdownMenu.Item>
+                    <DropdownMenu.Item>Logout</DropdownMenu.Item>
+                </div>
+            </DropdownMenu.Content>
+        </DropdownMenu.Root>
+      </div>
+
+      {/* main content */}
+      <div>
+        {/* nav bar */}
+        <nav>
+            <div>
+                <NavLink>
+                    <FaListCheck />
+                    <span>
+                        dashboard
+                    </span>
+                </NavLink>
+                
+                <NavLink>
+                    <FaWallet />
+                    <span>
+                        portfolio
+                    </span>
+                </NavLink>
+                
+                <NavLink>
+                    <FaStar />
+                    <span>
+                        watchlist
+                    </span>
+                </NavLink>
+                
+                <NavLink>
+                    <FaUser />
+                    <span>
+                        profile
+                    </span>
+                </NavLink>
+            </div>
+
+        </nav>
+
+        {/* content */}
+        <main>
+          <Outlet />
+        </main>
+      </div>
     </div>
   )
 }
