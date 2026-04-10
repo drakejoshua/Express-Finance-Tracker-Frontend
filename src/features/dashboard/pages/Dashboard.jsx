153b9230fc5eae2b95eb17c40ae3b1d0a665d9d7
Joshua Mabawonku <drakejoshua086@gmail.com>
2026-04-11 00:19:18 +0100

    feat( dashboard ): add markup and content to the dashboard page
    This commit adds the basic structure and content to the dashboard page,
    including a welcome message and placeholders for future components. The
    dashboard will serve as the main landing page for users after logging in,
    providing an overview of their activities and quick access to important features.
    This is the initial setup for the dashboard, and further enhancements
    such as: styling, theming, etc; will be made in subsequent commits.

--- src/features/dashboard/pages/Dashboard.jsx
+++ src/features/dashboard/pages/Dashboard.jsx
@@ -1,12 +1,198 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
+import bitcoinImage from '../../../assets/Design/bitcoin.png'
+import { FaArrowDown, FaCaretDown, FaPen, FaTrash } from 'react-icons/fa6'
+import bitcoinImage from "../../assets/Design/bitcoin.png"
 
 export default function Dashboard() {
-  return (
-    <div>
-      This is the dashboard page
+    return (
+        <div>
+            {/* dashboard content */}
+            <div>
+                {/* greeting */}
+                <h1>
+                    Welcome, Joshua
+                </h1>
 
-      <Outlet />
-    </div>
-  )
+                {/* portfolio overview */}
+                <div>
+                    <h2>
+                        Portfolio Overview
+                    </h2>
+
+                    {/* portfolio chart */}
+                    <div>
+                    </div>
+
+                    {/* portfolio stats */}
+                    <div>
+                        <div>
+                            <h3>top gainers</h3>
+
+                            {/* top 3 gainers */}
+                            <div>
+                                {/* item */}
+                                <div>
+                                    <img src={ bitcoinImage } alt="bitcoin image" />
+
+                                    <div>
+                                        <span>
+                                            bitcoin
+                                        </span>
+
+                                        <span>
+                                            <span>
+                                                $28,000
+                                            </span>
+                                            
+                                            <span>
+                                                <FaCaretDown />
+
+                                                <span>
+                                                    -5.2%
+                                                </span>
+                                            </span>
+                                        </span>
+                                    </div>
+
+                                    <span>
+                                        -$1,500
+                                    </span>
+                                </div>
+                            </div>
+                        </div>
+                        
+                        {/* top 3 losers */}
+                        <div>
+                            <h3>top losers</h3>
+
+                            <div>
+                                {/* TODO: add items after styling */}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                {/* watchlist overview */}
+                <h2>
+                    Recent Watchlist & Alerts
+                </h2>
+                
+                <div>
+                    {/* watchlist stats */}
+                    <div>
+                        <h3>top gainers</h3>
+
+                        {/* top 3 gainers */}
+                        <div>
+                            {/* item */}
+                            <div>
+                                <img src={ bitcoinImage } alt="bitcoin image" />
+
+                                <div>
+                                    <span>
+                                        bitcoin
+                                    </span>
+
+                                    <span>
+                                        $28,000
+                                    </span>
+                                </div>
+
+                                <span>
+                                    <FaCaretDown />
+
+                                    <span>
+                                        -5.2%
+                                    </span>
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                    
+                    {/* top 3 losers */}
+                    <div>
+                        <h3>top losers</h3>
+
+                        <div>
+                            {/* TODO: add items after styling */}
+                        </div>
+                    </div>
+
+                    {/* alerts */}
+                    <div>
+                        {/* alert */}
+                        <div
+                            className="
+                                flex
+                                gap-2
+                                items-center
+                            "
+                        >
+                            <img 
+                                src={ bitcoinImage } 
+                                alt="image of bitcoin" 
+                                className="
+                                    w-10
+                                    h-10
+                                "
+                            />
+
+                            <div>
+                                <span
+                                    className="
+                                        text-lg
+                                    "
+                                >
+                                    Bitcoin Alert
+                                </span>
+
+                                <span
+                                    className="
+                                        -mt-1  
+                                        flex
+                                        gap-1
+                                        items-center
+                                    "
+                                >
+                                    <FaArrowDown
+                                        className="
+                                            text-red-500
+                                        "
+                                    />
+
+                                    <span>
+                                        $79000
+                                    </span>
+                                </span>
+                            </div>
+
+                            <div
+                                className="
+                                    ml-auto
+                                    flex
+                                    gap-2
+                                    items-center
+                                    *:text-white
+                                    *:bg-green-700 *:hover:bg-green-900
+                                    *:p-2
+                                    *:rounded-full
+                                "
+                            >
+                                <button>
+                                    <FaPen/>
+                                </button>
+                                
+                                <button>
+                                    <FaTrash/>
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <Outlet />
+        </div>
+    )
 }
