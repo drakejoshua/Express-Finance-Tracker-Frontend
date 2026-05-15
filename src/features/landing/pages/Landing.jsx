e7e0f3d0507d9a86316d042ce9d3e132384d3e95
Joshua Mabawonku <drakejoshua086@gmail.com>
2026-05-16 00:56:13 +0100

    feat( landing ): add markup, styling, and responsiveness to landing page
    This commit introduces the structure and design for the landing page
    of our application. The landing page now includes a logo, a headline,
    a descriptive paragraph, and a call-to-action button. The layout is
    responsive, ensuring that it looks good on both mobile and desktop
    devices. Tailwind CSS classes have been used to style the components
    and manage spacing effectively. This sets the foundation for further
    enhancements and content additions to the landing page in future commits.

--- src/features/landing/pages/Landing.jsx
+++ src/features/landing/pages/Landing.jsx
@@ -1,13 +1,87 @@
-import { useDialogProvider } from '../../../shared/providers/DialogProvider.jsx'
-import { useTheme } from '../../../shared/providers/ThemeProvider.jsx'
+import { useNavigate } from "react-router-dom"
+import Logo from "../../../shared/components/Logo.jsx"
+import Button from "../../../shared/components/Button.jsx"
+import AltButton from "../../../shared/components/AltButton.jsx"
+import ThemeButton from "../../../shared/components/ThemeButton.jsx"
 
 export default function Landing() {
-    const { toggleTheme } = useTheme()
+    const navigateTo = useNavigate()
 
     return (
-        <div>
-            <button className='text-xl' onClick={ () => toggleTheme() }> toggle theme </button>
-            This is the landing page
+        <div
+            className="
+                py-16
+                px-2
+                w-9/10
+                max-w-4xl
+                mx-auto
+            "
+        >
+            <h1 
+                className="
+                    text-xl lg:text-3xl
+                    font-medium
+                    text-center
+                    text-gray-700 dark:text-white
+                "
+            >
+                Welcome to 
+            </h1>
+
+            <Logo 
+                className="
+                    h-auto
+                    mt-4
+                "
+            />
+
+            <p 
+                className="
+                    mt-8
+                    text-center
+                    md:max-w-xl lg:max-w-2xl
+                    mx-auto
+                    lg:text-lg
+                    dark:text-white
+                "
+            >
+                The landing page is still under construction, 
+                but you can sign up or log in to access the full 
+                features of our application. we provide a seamless 
+                experience for tracking your crypto assets in any currency, 
+                and we are constantly working to improve our platform to 
+                meet your needs.
+            </p>
+
+            <div 
+                className="
+                    mt-8 lg:mt-12
+                    flex
+                    gap-4
+                    w-fit
+                    mx-auto
+                "
+            >
+                <Button
+                    className="
+                        px-6
+                    "
+                    onClick={() => navigateTo("/auth/signup")}
+                >
+                    Sign up
+                </Button>
+
+                <AltButton
+                    className="
+                        px-6
+                    "
+                    onClick={() => navigateTo("/auth/login")}
+                >
+                    Log in
+                </AltButton>
+
+                <ThemeButton/>
+            </div>
         </div>
     )
 }
